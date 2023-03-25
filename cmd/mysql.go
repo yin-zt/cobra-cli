14f92beb5eb2c493299927b3f2bce028dd4a89fb
yin-zt <15626499421@163.com>
2023-03-25 23:55:44 +0800

    增加mysql工具，支持连接mysql后执行原生的sql语句，并返回受影响的行数

--- cmd/mysql.go
+++ cmd/mysql.go
@@ -7,17 +7,18 @@ import (
 
 func init() {
 	RootCmd.AddCommand(mysqlCmd)
-	mysqlCmd.Flags().StringP("username", "u", "root", "./net-tools mysql -u root -p 123456")
-	mysqlCmd.Flags().StringP("password", "p", "123456", "net-tools mysql -u root -p 123456")
+	mysqlCmd.Flags().StringP("username", "u", "root", "./cli mysql -u root -p 123456 -c sql")
+	mysqlCmd.Flags().StringP("password", "p", "123456", "cli mysql -u root -p 123456 -c sql")
+	mysqlCmd.Flags().StringP("command", "c", "", "net-tools mysql -u root -p 123456 -c sql")
 }
 
 var mysqlCmd = &cobra.Command{
 	Use:   "mysql",
 	Short: "Simulate a mysql client to connect to the database",
-	Long:  "net-tools mysql 127.0.0.1:3306 -u root -p 123456",
+	Long:  "cli mysql 127.0.0.1:3306 -u root -p 123456",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) != 1 {
-			log.Fatalln("You have entered the wrong parameter, Usage: ./net-tools mysql 127.0.0.1:3306 -u root -p 123456")
+			log.Fatalln("You have entered the wrong parameter, Usage: ./net-tools mysql 127.0.0.1:3306 -u root -p 123456 -c sql")
 		}
 		username, err := cmd.Flags().GetString("username")
 		if err != nil {
@@ -27,9 +28,13 @@ var mysqlCmd = &cobra.Command{
 		if err != nil {
 			log.Fatalln(err)
 		}
-		if err := cli.Util.MysqlPingCheck(args[0], username, password); err != nil {
+		rawsql, err := cmd.Flags().GetString("command")
+		if err != nil {
 			log.Fatalln(err)
 		}
-		log.Printf("mysql %s connection successful", args[0])
+		if err := cli.Util.MysqlPingCheck(args[0], username, password, rawsql); err != nil {
+			log.Fatalln(err)
+		}
+		log.Printf("mysql %s exec sql successful", args[0])
 	},
 }
--- core/mysql.go
+++ core/mysql.go
@@ -2,12 +2,13 @@ package core
 
 import (
 	"context"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
 	"time"
 )
 
-func (this *Common) MysqlPingCheck(host, username, password string) error {
+func (this *Common) MysqlPingCheck(host, username, password, command string) error {
 	en, err := xorm.NewEngine("mysql", username+":"+password+"@tcp("+host+")/mysql?charset=utf8&parseTime=true")
 	defer en.Close()
 	if err != nil {
@@ -15,8 +16,19 @@ func (this *Common) MysqlPingCheck(host, username, password string) error {
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
-	if err = en.PingContext(ctx); err != nil {
-		return err
+	if command == "" {
+		if err = en.PingContext(ctx); err != nil {
+			return err
+		}
+		return nil
+	} else {
+		if result, err := en.Exec(command); err == nil {
+			count, _ := result.RowsAffected()
+			fmt.Sprintf("rowsAffected number: %d", count)
+			return nil
+		} else {
+			return err
+		}
 	}
-	return nil
+
 }
