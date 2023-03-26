889260243ac96e0c8abe24413391b489af025679
yin-zt <15626499421@163.com>
2023-03-26 09:57:15 +0800

    redis命令支持连接及执行redis命令

--- cmd/redis.go
+++ cmd/redis.go
@@ -8,28 +8,31 @@ import (
 
 func init() {
 	RootCmd.AddCommand(redisCmd)
-	redisCmd.Flags().StringP("password", "p", "", "./net-tools redis -p 123456")
+	redisCmd.Flags().StringP("password", "p", "", "./cli redis -p 123456 -c command")
+	redisCmd.Flags().StringP("command", "c", "", "./cli redis -p 123456 -c command")
 }
 
 var redisCmd = &cobra.Command{
 	Use:   "redis",
 	Short: "Simulating a redis client connection to redis",
-	Long:  "net-tools redis 123456",
+	Long:  "cli redis -p 123456 -c command",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) != 1 {
-			log.Fatalln("You have entered the wrong parameter, Usage: ./net-tools redis 127.0.0.1:6379 -p 123456")
+			log.Fatalln("You have entered the wrong parameter, Usage: ./cli redis 127.0.0.1:6379 -p 123456 -c command")
 		}
 		host := args[0]
 		if len(strings.Split(host, ":")) != 2 {
-			log.Fatalln("You have entered the wrong parameter, Usage: ./net-tools redis 127.0.0.1:6379 -p 123456")
+			log.Fatalln("You have entered the wrong parameter, Usage: ./cli redis 127.0.0.1:6379 -p 123456 -c command")
 		}
 		password, err := cmd.Flags().GetString("password")
 		if err != nil {
 			log.Fatalln(err)
 		}
-		if err := cli.Util.Redischeck(host, password); err != nil {
+		commands, err := cmd.Flags().GetString("command")
+
+		if err := cli.Util.Redisexec(host, password, commands); err != nil {
 			log.Fatalln(err)
 		}
-		log.Printf("redis %s connection successful", host)
+		log.Printf("redis %s exec comamnd successfully", host)
 	},
 }
--- core/redis.go
+++ core/redis.go
@@ -1,18 +1,71 @@
 package core
 
 import (
+	"fmt"
 	"github.com/go-redis/redis"
+	"strings"
 )
 
-func (this *Common) Redischeck(redishost, redispassword string) error {
+func (this *Common) Redisexec(redisHost, redisPassword, command string) error {
 	client := redis.NewClient(&redis.Options{
-		Addr:     redishost,
-		Password: redispassword, // no password set
+		Addr:     redisHost,
+		Password: redisPassword, // no password set
 		DB:       0,             // use default DB
 	})
-	_, err := client.Ping().Result()
-	if err != nil {
-		return err
+	var args []string
+	if command == "" {
+		if _, err := client.Ping().Result(); err != nil {
+			return err
+		} else {
+			return nil
+		}
+	} else {
+		args = strings.Split(command, " ")
+	}
+	//result := client.Do("SMEMBERS", "letters")
+	switch count := len(args); count {
+	case 1:
+		result := client.Do(args[0])
+		resStr, err := result.Result()
+		if err != nil {
+			return err
+		} else {
+			fmt.Println(resStr)
+			return nil
+		}
+	case 2:
+		result := client.Do(args[0], args[1])
+		resStr, err := result.Result()
+		if err != nil {
+			return err
+		} else {
+			fmt.Println(resStr)
+			return nil
+		}
+	case 3:
+		result := client.Do(args[0], args[1], args[2])
+		resStr, err := result.Result()
+		if err != nil {
+			return err
+		} else {
+			fmt.Println(resStr)
+			return nil
+		}
 	}
 	return nil
 }
+
+//func InitRedisPool() (redis.Conn, error) {
+//	conn, err := redis.Dial("tcp", "127.0.0.1:6380",
+//		redis.DialConnectTimeout(time.Duration(2)*time.Second),
+//		redis.DialPassword("de477b4a-ef25-7cf9-8098-ee9d1245dc7f"),
+//		redis.DialDatabase(0),
+//	)
+//	if err != nil {
+//		fmt.Println(err)
+//		//log.Error(err)
+//		fmt.Println(err)
+//	}
+//	return conn, err
+//
+//}
