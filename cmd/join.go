fcb774edf8b45a85f66e40101c53631e4f5c1c05
yin-zt <15626499421@163.com>
2023-04-01 00:37:38 +0800

    增加join功能,支持拼接列表。

---
+++ cmd/join.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(JoinCmd)
+	JoinCmd.Flags().StringP("strings", "s", "-", "--strings")
+	JoinCmd.Flags().StringP("word", "w", "", "--word")
+}
+
+var JoinCmd = &cobra.Command{
+	Use:   "join",
+	Short: "Simulate the join command to integate some substring",
+	Long:  "cli join -s '-' -w '$$'",
+	Run: func(cmd *cobra.Command, args []string) {
+		joinStr, err := cmd.Flags().GetString("strings")
+		if err != nil {
+			cmdlog.Error(err)
+		}
+		joinWord, err := cmd.Flags().GetString("word")
+		if err != nil {
+			cmdlog.Error(err)
+		}
+		cli.Util.Join(joinStr, joinWord)
+	},
+}
---
+++ core/join.go
@@ -0,0 +1,33 @@
+package core
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Join 使用方法是 echo '["aa", "bb", "cc"]' | gmd join -s "-" -w "GG"
+// 作用是将输出的字符串列表使用指定字符拼接，-w表示列表元素拼接前在其前后端加上val，-t参数目前没有作用
+func (this *Common) Join(joinS, joinW string) {
+	obj, _ := this.StdinJson()
+	sep := joinS
+	wrap := joinW
+	trim := ""
+	//argv := utils.GetArgsMap()
+	var lines []string
+	switch obj.(type) {
+	case []interface{}:
+		for _, v := range obj.([]interface{}) {
+			if trim != "" {
+				if v == nil || fmt.Sprintf("%s", v) == "" {
+					continue
+				}
+			}
+			if wrap != "" {
+				lines = append(lines, fmt.Sprintf("%s%s%s", wrap, v, wrap))
+			} else {
+				lines = append(lines, fmt.Sprintf("%s", v))
+			}
+		}
+	}
+	fmt.Println(strings.Join(lines, sep))
+}
