a2e68f710a52debea2ecaa337f1ccb39dbb7d3ab
yin-zt <15626499421@163.com>
2023-03-31 09:07:02 +0800

    增加len功能,用于找出map字典中所有的key个数或者输入字符串的长度；仅支持获取交互输入或者echo xxx | cli len

---
+++ cmd/len.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(LenCmd)
+}
+
+var LenCmd = &cobra.Command{
+	Use:   "len",
+	Short: "Simulate the len command to figure out the length of object ",
+	Long:  "echo '{\"key1\": \"val1\", \"key2\": \"val2\"}' | cli len",
+	Run: func(cmd *cobra.Command, args []string) {
+		cli.Util.Len()
+	},
+}
---
+++ core/len.go
@@ -0,0 +1,26 @@
+package core
+
+import (
+	"fmt"
+)
+
+// Len 使用方法是：echo "aabbcc" | gmd len  或者  echo '{"key1": "val1", "key2": "val2"}' | gmd len
+// 作用是返回输出字符串长度或者类字典数据中key的个数
+func (this *Common) Len() {
+	obj, in := this.StdinJson()
+	switch obj.(type) {
+	case []interface{}:
+		fmt.Println(len(obj.([]interface{})))
+		return
+
+	case map[string]interface{}:
+		i := 0
+		for _ = range obj.(map[string]interface{}) {
+			i = i + 1
+		}
+		fmt.Println(i)
+		return
+	}
+
+	fmt.Println(len(in))
+}
